1f6c1d5a1287e9ff62de6e4f81e53bac3e5c1e91
Philippe Marschall <philippe.marschall@gmail.com>
2022-12-16 17:34:32 +0100

    bind json object

--- src/test/java/com/github/marschall/openworld2022/OjdbcJsonTests.java
+++ src/test/java/com/github/marschall/openworld2022/OjdbcJsonTests.java
@@ -3,7 +3,10 @@ package com.github.marschall.openworld2022;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.sql.SQLException;
+import java.util.Map;
 
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.sql.DataSource;
 
 import org.junit.jupiter.api.Test;
@@ -28,7 +31,8 @@ class OjdbcJsonTests {
     try (var connection = this.dataSource.getConnection();
          var preparedStatement = connection.prepareStatement("""
              SELECT JSON('{"projectId": "NCA-029-0"}')
-             FROM dual"""); // no longer needed on 23
+             FROM dual
+             """); // no longer needed on 23
 //        var preparedStatement = connection.prepareStatement("""
 //             SELECT JSON({'projectId': 'NCA-029-0'})
 //             FROM dual"""); // no longer needed on 23
@@ -45,8 +49,19 @@ class OjdbcJsonTests {
   void jdbcTemplateJsonObject() {
     javax.json.JsonObject doc = this.jdbcOperations.queryForObject("""
              SELECT JSON('{"projectId": "NCA-029-0"}')
-             FROM dual""", javax.json.JsonObject.class);
+             FROM dual
+             """, javax.json.JsonObject.class);
     assertEquals("NCA-029-0", doc.getString("projectId"));
+  }
+
+  @Test
+  void bindJson() {
+    JsonObject jsonObject = Json.createObjectBuilder(Map.of("projectId", "NCA-029-0")).build();
+    String projectId = this.jdbcOperations.queryForObject("""
+             SELECT JSON_VALUE(?, '$.projectId')
+             FROM dual
+             """, String.class, jsonObject);
+    assertEquals("NCA-029-0", projectId);
   }
 
   @Test
@@ -54,7 +69,8 @@ class OjdbcJsonTests {
     String projectId = this.jdbcOperations.queryForObject("""
              SELECT *
              FROM JSON_TABLE('{"projectId": "NCA-029-0"}',
-                 '$' COLUMNS (projectId VARCHAR2(9)))""", String.class);
+                 '$' COLUMNS (projectId VARCHAR2(9)))
+             """, String.class);
     assertEquals("NCA-029-0", projectId);
   }
 
